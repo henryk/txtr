65b97c1a853841e80e9b25faba45e710053c8a1d
hploetz <hploetz@a3ebe3ea-ee07-45ba-8c8b-080dba9f4f68>
2009-05-04 03:19:09 +0000

    Add code to use httplib2 if possible. This will automatically use Keep-Alive for requests if possible. Simplistic fallback to urllib is in place (will need some work for use with the delivery API)
    git-svn-id: file:///home/henryk/svn/txtr/trunk@13 a3ebe3ea-ee07-45ba-8c8b-080dba9f4f68

--- txtr.py
+++ txtr.py
@@ -1,13 +1,69 @@
 import urllib, sys, sha, random
+
 try:
     import simplejson
 except ImportError:
     print >>sys.stderr, "Could not import simplejson, please install it"
     raise
 
+try:
+    import httplib2
+except ImportError:
+    pass
+
+class _HTTP_Wrapper(object):
+    """A simple http wrapper object to try to use httplib2 if possible, and fall back to urllib otherwise.
+    Try to handle Keep-Alive if possible. Try to handle singleton-ish behaviour, if possible."""
+    _CACHE = {}
+    
+    @classmethod
+    def new(clazz, cookie = None, baseurl = None, *args, **kwargs):
+        """Get a _HTTP_Wrapper object. Will use (cookie, baseurl) as a cache key
+        to try to return a previously created object iff both are not-None.
+        The idea is to only have one Keep-Alive connection (if possible) per instantiation context
+        (e.g. this module) and usage (e.g. JSON vs. delivery)."""
+        
+        def make_new(): return clazz(cookie=cookie, baseurl=baseurl, *args, **kwargs)
+        
+        if cookie is None or baseurl is None:
+            return make_new()
+            
+        else:
+            try:
+                return clazz._CACHE[ (cookie, baseurl) ]
+            except TypeError:
+                return make_new()
+            except KeyError:
+                clazz._CACHE[ (cookie, baseurl) ] = v = make_new()
+                return v
+    
+    def __init__(self, cookie, baseurl, timeout = None):
+        
+        if "httplib2" in sys.modules:
+            self.httplib2 = httplib2.Http(timeout = timeout)
+        else:
+            self.httplib2 = None
+    
+    def request(self, uri, method = "GET", body = None, headers = None):
+        if self.httplib2 is not None:
+            response, body = self.httplib2.request(uri, method=method, body=body, headers=headers)
+            return (response, body)
+        else:
+            if method != "GET" or headers is not None or body is not None:
+                raise NotImplementedError, "Fallback for advanced HTTP operation is not yet implemented"
+            
+            fp = urllib.urlopen(uri)
+            body = fp.read()
+            fp.close()
+            
+            return (None, body)
+
 class _JSONBASE(object):
     RPCURL = "http://txtr.com/json/rpc"
     DEBUG = 0
+    
+    def __init__(self, cookie = None):
+        self.json_http_wrapper = _HTTP_Wrapper.new(cookie = cookie, baseurl = self.RPCURL)
     
     def __getattr__(self, fname):
         return lambda *args, **kwargs: self._docall(fname, *args, **kwargs)
@@ -48,7 +104,7 @@ class _JSONBASE(object):
         if self.DEBUG:
             print ">> ", urllib.unquote(callurl)
         
-        r = urllib.urlopen(callurl).read()
+        _, r = self.json_http_wrapper.request(callurl)
         response = simplejson.loads(r)
         
         if self.DEBUG:
@@ -62,20 +118,21 @@ class _JSONBASE(object):
             else:
                 return response["result"]
 
+_COOKIE = object()
 class _WSAuth(_JSONBASE):
     BASENAME = "WSAuth"
     METHODS = {
         "authenticateUserByName": ["userName", "password", "stickyToken"],
         "deAuthenticate": ["token"],
     }
-WSAuth = _WSAuth()
+WSAuth = _WSAuth(_COOKIE)
 
 class _WSViewMgmt(_JSONBASE):
     BASENAME = "WSViewMgmt"
     METHODS = {
         "getViewSets": ["token", "viewSetsOfUser"],
     }
-WSViewMgmt = _WSViewMgmt()
+WSViewMgmt = _WSViewMgmt(_COOKIE)
 
 class _WSListMgmt(_JSONBASE):
     BASENAME = "WSListMgmt"
@@ -85,7 +142,7 @@ class _WSListMgmt(_JSONBASE):
         "getSpecialList": ["token", "specialListType", "offset", "count"],
         "addDocumentsToList": ["token", "listID", "documentIDs", "addAt"],
     }
-WSListMgmt = _WSListMgmt()
+WSListMgmt = _WSListMgmt(_COOKIE)
 
 class _WSDocMgmt(_JSONBASE):
     BASENAME = "WSDocMgmt"
@@ -100,14 +157,14 @@ class _WSDocMgmt(_JSONBASE):
         "removeDocumentAttributeCategory": ["token", "documentIDs", "categoryID"],
         "changeDocumentAttributes": ["token", "documentIDs", "attributes"]
     }
-WSDocMgmt = _WSDocMgmt()
+WSDocMgmt = _WSDocMgmt(_COOKIE)
 
 class _WSUserMgmt(_JSONBASE):
     BASENAME = "WSUserMgmt"
     METHODS = {
         "getUserSettings": ["token"],
     }
-WSUserMgmt = _WSUserMgmt()
+WSUserMgmt = _WSUserMgmt(_COOKIE)
 
 class txtr(object):
     def __init__(self, username=None, password=None, passhash=None, auth_from=None):
