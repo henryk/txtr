6f857ec9675be489ad8c95f634aa807f41f57e3b
hploetz <hploetz@a3ebe3ea-ee07-45ba-8c8b-080dba9f4f68>
2009-05-27 03:54:10 +0000

    Add setup.py (not working yet)
    git-svn-id: file:///home/henryk/svn/txtr/trunk@61 a3ebe3ea-ee07-45ba-8c8b-080dba9f4f68

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='txtr Uploader',
+    version='0.5',
+    description='txtr Uploader for Linux',
+    author='Henryk Plötz',
+    author_email='henryk@ploetzli.ch',
+    url="http://svn.ploetzli.ch/txtr/",
+    
+    install_requires=["simplejson"],
+    
+    package_dir={'':'src'},
+    packages=["txtr", "txtr/gui"],
+    
+    package_data={
+        "": ["bg_txtrSynchronizer.png", "uploader_logo.png", "uploader.glade"],
+    },
+    include_package_data=False,
+    zip_safe=False,
+    
+    entry_points = {
+        "gui_scripts": [
+            "txtr_uploader = txtr.gui.uploader:main",
+        ],
+    }
+)
